10b61e3d63d09247a185eadfe2dcd2e4563f4301
max jn <maxjn763@gmail.com>
2022-12-14 16:29:20 +0330

    sidebar widget files got seprated "taxonomy-list-item.php" was created to show a list of choocen taxonomies in widgets 'links' in "blog-list-small-body.php" got fixed

--- templates/card/blog/content/blog-list-small-body.php
+++ templates/card/blog/content/blog-list-small-body.php
@@ -10,7 +10,7 @@
 ?>
 <div class="col-sm-8 mt-3 mt-sm-0">
     <h5 class="h6">
-        <a class="link-title" href="blog-single.html"><?php the_title() ?></a>
+        <a class="link-title" href="<?php the_permalink() ?>"><?php the_title() ?></a>
     </h5>
-    <a class="d-inline-block text-muted" href="#"></i> <?php the_time('F j, Y'); ?></a>
+    <a class="d-inline-block text-muted" href="<?php the_permalink() ?>"></i> <?php the_time('F j, Y'); ?></a>
 </div>
---
+++ templates/widget/container/taxonomy-list.php
@@ -0,0 +1,8 @@
+      <!-- Categories  Start -->
+      <div class="mb-5 border-bottom border-light pb-5">
+          <h4 class="mb-3"><?= esc_html_e('Categories', 'taypo') ?></h4>
+          <ul class="list-unstyled">
+              <?php get_template_part('templates\widget\content\taxonomy-list-item') ?>
+          </ul>
+      </div>
+      <!-- Categories  End -->
