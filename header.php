244ff01a3df90e1f3b7768011a88ff029f661e87
Muhammad Haghnegahdar <104886566+maxjn@users.noreply.github.com>
2022-12-15 12:46:14 +0330

    -Search form got completed (#7)
    & added to toggle nav menu
    -"search.php" was created

--- header.php
+++ header.php
@@ -41,18 +41,24 @@
 
         <nav class="navbar navbar-dark bg-dark fixed-top">
             <div class="offcanvas offcanvas-end bg-dark" tabindex="-1" id="offcanvasNavbar">
+                <!-- close btn start -->
                 <div class="offcanvas-header">
                     <button type="button" class="btn-close bg-transparent fs-1 ms-auto" data-bs-dismiss="offcanvas"
                         aria-label="Close">
                         <i class="bi bi-x-octagon"></i>
                     </button>
                 </div>
+                <!-- close btn end -->
+
                 <div class="offcanvas-body px-md-10 px-4 py-8">
                     <img class="img-fluid side-logo mb-3"
                         src="<?= get_template_directory_uri() ?>/assets/libraries/images/logo-white.png" alt="">
                     <p class="mb-0 text-white lead">Taypo - Multipurpose Bootstrap5 Template is Most PowerFull template
                         2022 For Everyone, Start working with an company that provide everything you need to generate
                         awareness.</p>
+                    <?php
+                    get_template_part('templates\widget\container\search-form')
+                    ?>
                     <div class="form-info border-top border-dark pt-6 mt-6">
                         <h5 class="text-white border-bottom border-white d-inline-block">Contact info</h5>
                         <ul class="contact-info list-unstyled mt-4">
--- templates/widget/container/search-form.php
+++ templates/widget/container/search-form.php
@@ -1,6 +1,6 @@
 <!-- Search Start -->
-<form class="border-bottom border-light pb-5 row mb-5">
-    <input class="form-control me-2 col h-auto" type="search" placeholder="Search" aria-label="Search">
+<form class="border-bottom border-light pb-5 row mb-5 searchform" name="searchform" id="searchform">
+    <input class="form-control me-2 col h-auto" type="search" placeholder="Search" aria-label="Search" name="s">
     <button class="btn btn-primary col-auto" type="submit">
         <i class="bi bi-search"></i>
     </button>
