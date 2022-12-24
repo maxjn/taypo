fe7a5b9b713cf54ebe0696a7437a345d932ae78e
max jn <maxjn763@gmail.com>
2022-12-24 13:54:49 +0330

    - 2 Accordions got dynamic using acf ✓

--- templates/accordian/accordian1.php
+++ templates/accordian/accordian1.php
@@ -1,3 +1,10 @@
+<?php
+if (isset($args['fields'])) {
+    $fields = $args['fields'];
+}
+$img = $fields["image"];
+
+?>
 <!--faq start-->
 
 <section>
@@ -8,91 +15,52 @@
                     <div class="row align-items-center justify-content-between">
                         <div class="col-12 col-lg-6 mb-8 mb-lg-0">
                             <div>
-                                <h6 class="border-bottom border-dark border-2 d-inline-block">F.A.Q.</h6>
-                                <h2 class="font-w-6">Find All Your Related Questions</h2>
+                                <h6 class="border-bottom border-dark border-2 d-inline-block">
+                                    <?= $fields["sub_title"] ?></h6>
+                                <h2 class="font-w-6"><?= $fields["title"] ?></h2>
                             </div>
+                            <?php
+                            if ($img) {
+                            ?>
+                            <img class="hero-image" src="<?= $img["url"] ?>" alt="<?= $img["title"] ?>">
+                            <?php
+                            } else { ?>
+                            <!-- Image -->
                             <lottie-player
                                 src="https://lottie.host/f57c02a8-9265-4873-b44a-f00a5a068318/eDbndF0d2e.json"
                                 background="transparent.html" speed="1" style="width: auto; height: auto;" loop
                                 autoplay></lottie-player>
+                            <?php } ?>
+
                         </div>
                         <div class="col-12 col-lg-6 col-xl-5">
                             <div class="accordion" id="accordion">
+                                <?php
+
+                                if (have_rows('questions', 'options')) {
+                                    $counter = 1;
+                                    while (have_rows('questions', 'options')) {
+                                        the_row()
+                                ?>
                                 <div class="accordion-item border-0 py-3 rounded-4 mb-4">
-                                    <h3 class="accordion-header" id="heading1">
+                                    <h3 class="accordion-header" id="heading<?= $counter ?>">
                                         <button
-                                            class="accordion-button py-0 font-w-6 shadow-none border-0 mb-0 bg-transparent rounded text-dark"
-                                            type="button" data-bs-toggle="collapse" data-bs-target="#collapse1"
-                                            aria-expanded="true" aria-controls="collapse1"> Hoe Does The Free Trial ?
+                                            class="accordion-button py-0 font-w-6 shadow-none border-0 mb-0 bg-transparent rounded text-dark collapsed"
+                                            type="button" data-bs-toggle="collapse"
+                                            data-bs-target="#collapse<?= $counter ?>" aria-expanded="false"
+                                            aria-controls="collapse<?= $counter ?>"> <?= get_sub_field('question') ?>
                                         </button>
                                     </h3>
-                                    <div id="collapse1" class="accordion-collapse border-0 collapse show"
-                                        aria-labelledby="heading1" data-bs-parent="#accordion">
-                                        <div class="accordion-body pt-0 text-muted">Synergistically visualize
-                                            alternative content before cross functional core Rapidiously focused
-                                            benefits.</div>
+                                    <div id="collapse<?= $counter ?>" class="accordion-collapse border-0 collapse"
+                                        aria-labelledby="heading<?= $counter ?>" data-bs-parent="#accordion">
+                                        <div class="accordion-body pt-0 text-muted"><?= get_sub_field('answer') ?></div>
                                     </div>
                                 </div>
-                                <div class="accordion-item border-0 py-3 rounded-4 mb-4">
-                                    <h3 class="accordion-header" id="heading2">
-                                        <button
-                                            class="accordion-button py-0 font-w-6 shadow-none border-0 mb-0 bg-transparent rounded text-dark collapsed"
-                                            type="button" data-bs-toggle="collapse" data-bs-target="#collapse2"
-                                            aria-expanded="false" aria-controls="collapse2"> What are your social media
-                                            ? </button>
-                                    </h3>
-                                    <div id="collapse2" class="accordion-collapse border-0 collapse"
-                                        aria-labelledby="heading2" data-bs-parent="#accordion">
-                                        <div class="accordion-body pt-0 text-muted">Synergistically visualize
-                                            alternative content before cross functional core Rapidiously focused
-                                            benefits.</div>
-                                    </div>
-                                </div>
-                                <div class="accordion-item border-0 py-3 rounded-4 mb-4">
-                                    <h3 class="accordion-header" id="heading3">
-                                        <button
-                                            class="accordion-button py-0 font-w-6 shadow-none border-0 mb-0 bg-transparent rounded text-dark collapsed"
-                                            type="button" data-bs-toggle="collapse" data-bs-target="#collapse3"
-                                            aria-expanded="false" aria-controls="collapse3"> Which is the same as saying
-                                            through ? </button>
-                                    </h3>
-                                    <div id="collapse3" class="accordion-collapse border-0 collapse"
-                                        aria-labelledby="heading3" data-bs-parent="#accordion">
-                                        <div class="accordion-body pt-0 text-muted">Synergistically visualize
-                                            alternative content before cross functional core Rapidiously focused
-                                            benefits.</div>
-                                    </div>
-                                </div>
-                                <div class="accordion-item border-0 py-3 rounded-4 mb-4">
-                                    <h3 class="accordion-header" id="heading4">
-                                        <button
-                                            class="accordion-button py-0 font-w-6 shadow-none border-0 mb-0 bg-transparent rounded text-dark collapsed"
-                                            type="button" data-bs-toggle="collapse" data-bs-target="#collapse4"
-                                            aria-expanded="false" aria-controls="collapse4"> What are your social media
-                                            ? </button>
-                                    </h3>
-                                    <div id="collapse4" class="accordion-collapse border-0 collapse"
-                                        aria-labelledby="heading4" data-bs-parent="#accordion">
-                                        <div class="accordion-body pt-0 text-muted">Synergistically visualize
-                                            alternative content before cross functional core Rapidiously focused
-                                            benefits.</div>
-                                    </div>
-                                </div>
-                                <div class="accordion-item border-0 py-3 rounded-4">
-                                    <h3 class="accordion-header" id="heading5">
-                                        <button
-                                            class="accordion-button py-0 font-w-6 shadow-none border-0 mb-0 bg-transparent rounded text-dark collapsed"
-                                            type="button" data-bs-toggle="collapse" data-bs-target="#collapse5"
-                                            aria-expanded="false" aria-controls="collapse5"> What are your social media
-                                            ? </button>
-                                    </h3>
-                                    <div id="collapse5" class="accordion-collapse border-0 collapse"
-                                        aria-labelledby="heading5" data-bs-parent="#accordion">
-                                        <div class="accordion-body pt-0 text-muted">Synergistically visualize
-                                            alternative content before cross functional core Rapidiously focused
-                                            benefits.</div>
-                                    </div>
-                                </div>
+                                <?php
+                                        $counter++;
+                                    }
+                                }
+                                ?>
                             </div>
                         </div>
                     </div>
